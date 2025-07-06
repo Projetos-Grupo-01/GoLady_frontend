3acce41c174d62d717ba1c7ac65543cdf7ef67c8
Samuel <samueldjsferreira@gmail.com>
2025-07-06 15:55:31 -0300

    :sparkles: CRUD Veiculos

---
+++ src/models/veiculos/Veiculos.ts
@@ -0,0 +1,18 @@
+// import type Postagem from "./Viagem";
+
+export default interface Veiculos{
+
+id : number | undefined
+
+modelo : String,
+
+placa : String,
+
+categoria: String,
+
+velocidadeMedia : number,
+
+descricao?: string;
+
+// viagem : List
+}
