7dfdc97635b64953aa74023ffc593c27ca065377
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-06 19:22:15 -0300

    :boom: Revertendo mudanças rotas e funções da service

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import './App.css'
 import DeletarVeiculos from './components/veiculos/deletarveiculos/DeletarVeiculos'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import CadastroViagem from './components/cadastroviagem/CadastroViagem'
+import Sobre from './pages/sobre/sobre'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <>
+      <ToastContainer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+
+            <Route path="/deletarveiculo/:id" element={<DeletarVeiculos />} />
+
+            <Route path="/cadastrarviagem" element={<CadastroViagem />} />
+
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
