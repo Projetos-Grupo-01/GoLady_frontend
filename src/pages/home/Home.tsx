23ac7e6b3195954c94e82691851e206dd94566ab
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-06 19:23:05 -0300

    :lipstick: Ajustes visuais página home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,9 @@
+import ListaViagens from "../../components/viagens/listaviagens/ListaViagens";
+
 function Home() {
     return (
         <>
-            <div className="flex items-center justify-center w-full py-16 bg-white"> {/* Ajustado para um fundo branco e padding vertical */}
+            <div className="flex items-center justify-center w-full py-16 bg-white drop-shadow-lg"> {/* Ajustado para um fundo branco e padding vertical */}
                 <div className="container flex flex-col md:flex-row items-center justify-between mx-auto px-4 gap-8"> {/* Container flexível para as duas colunas */}
                     
                     {/* Coluna da Esquerda: Texto e Botão */}
@@ -11,7 +13,7 @@ function Home() {
                             <span className="text-[#115B69]">Go</span>
                             <span className="text-[#7D0700]">Lady</span>
                         </h1>
-                        <p className="text-3xl font Mooli text-gray-900 mb-6">
+                        <p className="text-3xl text-gray-900 mb-6 text-shadow-lg/10">
                             Do trabalho ao barzinho, seu trajeto em segurança!
                         </p>
                         <button className="px-8 py-3 bg-teal-600 text-white font-semibold rounded-lg shadow-md hover:bg-[#115B69] transition duration-300 cursor-pointer">
@@ -23,11 +25,12 @@ function Home() {
                         <img
                             src="/ImgCarro.png" 
                             alt="Carro GoLady"
-                            className="max-w-full h-auto rounded-lg shadow-lg" 
+                            className="w-full h-auto" 
                         />
                     </div>
                 </div>
             </div>
+            <ListaViagens />
         </>
     );
 }
