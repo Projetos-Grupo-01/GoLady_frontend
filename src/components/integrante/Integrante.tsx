ecd589bb9d3488b5871577d855c82d5da786a641
Renata Negrini <renatanegrinim@gmail.com>
2025-07-06 15:48:33 -0300

    feat: Add fotos dos integrantes, ajuste de cor do componente integrante e ajuste de texto e fonte em componente sobre

--- src/components/integrante/Integrante.tsx
+++ src/components/integrante/Integrante.tsx
@@ -18,16 +18,16 @@ function Integrante({ member }: IntegranteProps) {
       <img
         src={member.image}
         alt={`Foto de ${member.name}`}
-        className="w-40 h-40 object-cover rounded-full border-2 border-green-700 shadow-md"
+        className="w-40 h-40 object-cover rounded-full border-2 border-[#115B69] shadow-md"
       />
       <h3 className="mt-4 text-xl font-semibold text-gray-800">{member.name}</h3>
       <p className="text-gray-600">{member.role}</p>
-      <div className="flex gap-3 py-2">
-        <a href={member.linkedinUrl}>
-          <LinkedinLogoIcon size={32} color="#1e2939"/>
+      <div className="flex gap-3 py-2 text-gray-800">
+        <a href={member.linkedinUrl} className="hover:text-[#7D0700]">
+          <LinkedinLogoIcon size={32}/>
         </a>
-        <a href={member.githubUrl}>
-          <GithubLogoIcon size={32} color="#1e2939"/>
+        <a href={member.githubUrl} className="hover:text-[#7D0700]">
+          <GithubLogoIcon size={32} />
         </a>
       </div>
     </div>
