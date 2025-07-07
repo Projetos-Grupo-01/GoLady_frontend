7b6229507066b0afa9ec240f8a12c6054d28c7ab
Renata Negrini <renatanegrinim@gmail.com>
2025-07-07 10:00:09 -0300

    feat: Ajuste visual de texto no Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,11 +9,11 @@ function Footer() {
       background: "linear-gradient(90deg, rgba(250,211,196,1) 0%, rgba(255,255,255,1) 54%, rgba(187,213,204,1) 97%)"
       }}>
         <div className=" flex-col flex justify-center items-center gap-2 font-Mooli">
-          <p className="text-xl font-bold">
+          <p className="text-xl font-bold text-gray-800">
             GoLady | Copyright: {data}
           </p>
           <div className="flex gap-1 items-center">
-            <p className=" text-base">Fale conosco pelo GitHub</p>
+            <p className=" text-base text-gray-800">Fale conosco pelo GitHub</p>
             <a href="https://github.com/Projetos-Grupo-01" className="text-gray-800 hover:text-[#7D0700]"target="_blank">
             <GithubLogoIcon size={32}/>
             </a>
