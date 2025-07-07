9b97e892294aaab41eadc7aae9580f65e68612ef
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-07 14:32:04 -0300

    :lipstick: Alteração na estilização de card Veiculos

--- src/components/veiculos/cardveiculos/CardVeiculos.tsx
+++ src/components/veiculos/cardveiculos/CardVeiculos.tsx
@@ -7,15 +7,20 @@ interface CardVeiculosProps {
 
 function CardVeiculos({ veiculos }: CardVeiculosProps) {
     return (
-        <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>
+        <div className='shadow-2xl flex flex-col rounded-2xl overflow-hidden justify-between'>
+            <header className='py-2 px-6 bg-cyan-900 text-white font-bold text-2xl'>
                 Veiculos
             </header>
-            <p className='p-8 text-3xl bg-slate-200 h-full'>{veiculos.modelo}</p>
+
+            <div className="flex flex-col px-8 py-4 bg-zinc-100 gap-1.5">
+                <p className='text-3xl'>{veiculos.modelo}</p>
+                <p className='text-xl'>Categoria: {veiculos.categoria}</p>
+                <p className='text-xl'>Placa: {veiculos.placa}</p>
+            </div>
 
             <div className="flex">
                 <Link to={`/deletarveiculo/${veiculos.id}`} className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                    flex items-center justify-center transition delay-2'>
+                    flex items-center justify-center transition delay-2 py-2'>
                     Deletar
                 </Link>
                 <Link to={`/atualizarveiculo/${veiculos.id}`} className='text-slate-100 bg-cyan-500 hover:bg-cyan-700 w-full 
