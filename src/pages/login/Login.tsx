77117d0f424fb05bce8ac10b75fc0237a1024b3b
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-29 15:45:43 -0300

    :lipstick: Ajuste espaçamento do form Login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -42,80 +42,82 @@ function Login() {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center bg-[#D9E4DD] pt-22 md:pt-0 px-6">
-        <form
-          onSubmit={login}
-          className="form-glass w-full max-w-md p-6 rounded-xl flex flex-col gap-2"
-        >
-          <h2 className="text-[#001427] text-5xl text-center mb-2 font-bold">
-            Login
-          </h2>
-
-          {/* User */}
-          <div className="flex flex-col w-full">
-            <label htmlFor="usuario" className="font-bold">
-              Usuário
-            </label>
-            <input
-              type="text"
-              id="usuario"
-              name="usuario"
-              className="rounded-lg p-3 border border-slate-500 bg-white"
-              value={usuarioLogin.usuario}
-              onChange={atualizarEstado}
-              placeholder="maria.souza@email.com"
-            />
-          </div>
-          {/* Senha */}
-          <div className="flex flex-col w-full">
-            <label htmlFor="senha" className="font-bold">
-              Senha
-            </label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              placeholder="Senha"
-              className="rounded-lg p-3 border border-slate-500 bg-white w-full"
-              value={usuarioLogin.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                atualizarEstado(e)
-              }
-            />
-          </div>
-          {/* Botão de login */}
-          <button
-            type="submit"
-            className=" bg-[#115B69] hover:bg-[#115a69d8] text-white cursor-pointer m-2
-                        py-2 px-4 rounded-lg w-1/2 mx-auto flex justify-center items-center transition"
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center bg-[#D9E4DD] pt-22 md:pt-0">
+        <div className="px-6">
+          <form
+            onSubmit={login}
+            className="form-glass w-full max-w-md p-6 rounded-xl flex flex-col gap-2"
           >
-            {isLoading ? (
-              <RotatingLines
-                strokeColor="white"
-                strokeWidth="5"
-                animationDuration="0.75"
-                width="24"
-                visible={true}
+            <h2 className="text-[#001427] text-5xl text-center mb-2 font-bold">
+              Login
+            </h2>
+
+            {/* User */}
+            <div className="flex flex-col w-full">
+              <label htmlFor="usuario" className="font-bold">
+                Usuário
+              </label>
+              <input
+                type="text"
+                id="usuario"
+                name="usuario"
+                className="rounded-lg p-3 border border-slate-500 bg-white"
+                value={usuarioLogin.usuario}
+                onChange={atualizarEstado}
+                placeholder="maria.souza@email.com"
               />
-            ) : (
-              <span>Entrar</span>
-            )}
-          </button>
-
-          <hr className="border-slate-400 w-full" />
-
-          <p className="text-center text-lg text-[#001427]">
-            Ainda não tem uma conta?{" "}
-            <Link
-              to="/cadastro"
-              className="text-[#115B69] hover:underline font-semibold"
+            </div>
+            {/* Senha */}
+            <div className="flex flex-col w-full">
+              <label htmlFor="senha" className="font-bold">
+                Senha
+              </label>
+              <input
+                type="password"
+                id="senha"
+                name="senha"
+                placeholder="Senha"
+                className="rounded-lg p-3 border border-slate-500 bg-white w-full"
+                value={usuarioLogin.senha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              />
+            </div>
+            {/* Botão de login */}
+            <button
+              type="submit"
+              className=" bg-[#115B69] hover:bg-[#115a69d8] text-white cursor-pointer m-2
+                        py-2 px-4 rounded-lg w-1/2 mx-auto flex justify-center items-center transition"
             >
-              <span>Criar conta</span>
-            </Link>
-          </p>
-        </form>
+              {isLoading ? (
+                <RotatingLines
+                  strokeColor="white"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="24"
+                  visible={true}
+                />
+              ) : (
+                <span>Entrar</span>
+              )}
+            </button>
+
+            <hr className="border-slate-400 w-full" />
+
+            <p className="text-center text-lg text-[#001427]">
+              Ainda não tem uma conta?{" "}
+              <Link
+                to="/cadastro"
+                className="text-[#115B69] hover:underline font-semibold"
+              >
+                <span>Criar conta</span>
+              </Link>
+            </p>
+          </form>
+        </div>
+
         <div className="fundoLogin hidden lg:block"></div>
-        
       </div>
     </>
   );
